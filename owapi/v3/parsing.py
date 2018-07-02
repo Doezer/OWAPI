9bdc8f79e181479ea6f87a8b4129465dcf6ef71d
Doezer <Doezerx@gmail.com>
2018-07-02 20:07:13 +0200

    gt

--- owapi/v3/parsing.py
+++ owapi/v3/parsing.py
@@ -72,6 +72,7 @@ def bl_parse_stats(parsed, mode="quickplay", status=None):
             comprank = int(hasrank[0].text)
         else:
             comprank = None
+        print(comprank)
         built_dict["overall_stats"]["comprank"] = comprank
         return built_dict
 
