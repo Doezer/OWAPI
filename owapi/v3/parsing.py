0187332bf42777d73b0ce9ffc21be0f00d605621
Doezer <Doezerx@gmail.com>
2018-07-02 19:58:09 +0200

    gt

--- owapi/v3/parsing.py
+++ owapi/v3/parsing.py
@@ -67,9 +67,9 @@ def bl_parse_stats(parsed, mode="quickplay", status=None):
 
     # Shortcut location for player level etc
     if status:
-        mast_head = parsed.xpath(".//div[@class='masthead-player']")[0]
-    else:
         mast_head = parsed.xpath(".//div[@class='masthead-player']")
+    else:
+        mast_head = parsed.xpath(".//div[@class='masthead-player']")[0]
 
     # Get the prestige.
     prestige = mast_head.xpath(".//div[@class='player-level']")[0]
